44d11023ec0a91476df5b4acb8e1ee8e3430b1ba
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-12 14:02:46 +0300

    Add multiple saga example

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -5,6 +5,7 @@ import { createUserSaga } from './userSagas';
 
 function* rootSaga() {
   yield takeLatest(ACTION_TYPES.CREATE_USER_REQUEST, createUserSaga);
+  // yield takeLatest(ACTION_TYPES.GET_USERS_REQUEST, getUsersSaga);
 }
 
 export default rootSaga;
