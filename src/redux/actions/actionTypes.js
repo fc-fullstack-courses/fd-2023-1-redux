c9102291b63bf1bcf1d9557a9406ee2e6081c005
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-13 17:08:30 +0300

    Remove legacy redux code

--- src/redux/actions/actionTypes.js
+++
@@ -1,7 +0,0 @@
-const ACTION_TYPES = {
-  CREATE_USER_REQUEST: 'users/createRequest',
-  CREATE_USER_SUCCESS: 'users/createSuccess',
-  CREATE_USER_ERROR: 'users/createError'
-}
-
-export default ACTION_TYPES;
--- src/redux/actions/tasksActionCreators.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-export const createTask = createAction('tasks/createTask');
-
-export const updateTask = createAction('tasks/updateTask');
-
-export const deleteTask = createAction('tasks/deleteTask');
